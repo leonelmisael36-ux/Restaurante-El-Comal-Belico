dc5258714a5b4e2089cfd6a8b8f4d905c915ff87
leonel <leonelmisael36@gmail.com>
2026-05-27 10:42:44 -0600

    mover y instalar el conector

--- Proyecto_Restaurante/Form1.Designer.cs
+++ Proyecto_Restaurante/Form1.Designer.cs
@@ -34,19 +34,19 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(340, 172);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(335, 140);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(349, 192);
+            this.button1.Size = new System.Drawing.Size(262, 156);
             this.button1.TabIndex = 0;
             this.button1.Text = "prueba";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(408, 106);
+            this.button2.Location = new System.Drawing.Point(306, 86);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(195, 23);
+            this.button2.Size = new System.Drawing.Size(146, 19);
             this.button2.TabIndex = 1;
             this.button2.Text = "Presiona aqui si eres chuy";
             this.button2.UseVisualStyleBackColor = true;
@@ -54,12 +54,11 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
