9b9d635bc0a7102ec85a6fdb2bd2c11ee51273a3
Leonel Misael <leonelmisael36@gmail.com>
2026-05-29 11:15:34 -0600

    Agregar clases a la biblioteca Logica

---
+++ CapaLogica/CL_Provee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapaLogica
+{
+    internal class CL_Provee
+    {
+    }
+}
---
+++ CapaLogica/Cl_Correo_Proveedor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapaLogica
+{
+    internal class Cl_Correo_Proveedor
+    {
+    }
+}
