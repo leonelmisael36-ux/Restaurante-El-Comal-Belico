f62d756b169962772c7835a03df17cff4cd68d2e
leonelmisael36-ux <leonemisael36@gmail.com>
2026-05-29 19:24:55 -0600

    si

--- CapaLogica/CL_Telefono_Cliente.cs
+++ CapaLogica/CL_Telefono_Cliente.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-}using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CapaLogica
