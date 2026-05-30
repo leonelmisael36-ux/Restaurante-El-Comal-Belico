687846ca6a469cbd608fe7a330b9401dc082a510
leonelmisael36-ux <leonemisael36@gmail.com>
2026-05-29 19:48:44 -0600

    ya

--- CapaM/CL_Pedido.cs
+++ CapaM/CL_Pedido.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Collections.Generic;
 using System.Linq;
-
+using System.Text;
+using System.Threading.Tasks;
 namespace CapaLogica
 {
     public class CL_Pedido
