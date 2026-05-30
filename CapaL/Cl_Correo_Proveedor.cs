6241340a51c5d0ca7081450fbbb3f77ed78b2232
leonelmisael36-ux <leonemisael36@gmail.com>
2026-05-29 19:42:02 -0600

    ahora si

---
+++ CapaL/Cl_Correo_Proveedor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CapaLogica
+{
+    public class Cl_Correo_Proveedor
+    {
+    }
+}
