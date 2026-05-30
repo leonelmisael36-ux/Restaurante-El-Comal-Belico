51256530aa365b420a7775a824f0d12fea7f879f
leonelmisael36-ux <leonemisael36@gmail.com>
2026-05-29 20:14:24 -0600

    mover bt

--- Proyecto_Restaurante/Reportes.Designer.cs
+++ Proyecto_Restaurante/Reportes.Designer.cs
@@ -33,9 +33,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(364, 134);
+            this.button1.Location = new System.Drawing.Point(300, 159);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -43,10 +44,11 @@
             // 
             // Reportes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Reportes";
             this.Text = "Reportes";
             this.ResumeLayout(false);
