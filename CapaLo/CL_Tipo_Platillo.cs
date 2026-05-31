1303714864771d0c28601e945a8abeca9460d036
leonelmisael36-ux <leonemisael36@gmail.com>
2026-05-31 09:54:56 -0600

    clases terminadas al 100%

--- CapaLo/CL_Tipo_Platillo.cs
+++ CapaLo/CL_Tipo_Platillo.cs
@@ -1,12 +1,175 @@
-﻿using System;
+﻿using CapaMo;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static CapaMo.ConexChuy;
 
 namespace CapaLogica
 {
     public class CL_Tipo_Platillo
     {
+        public List<Tipo_Platillo> Listar()
+        {
+            List<Tipo_Platillo> lista = new List<Tipo_Platillo>();
+
+            using (MySqlConnection conexion = new MySqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    string query = "SELECT * FROM tipo_platillo";
+
+                    MySqlCommand cmd = new MySqlCommand(query, conexion);
+                    cmd.CommandType = System.Data.CommandType.Text;
+
+                    conexion.Open();
+
+                    using (MySqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new Tipo_Platillo()
+                            {
+                                Id_Tipo = Convert.ToInt32(dr["Id_Tipo"]),
+                                Id_Platillo = Convert.ToInt32(dr["Id_Platillo"]),
+                                Tamaño = dr["Tamano"].ToString(),
+                                TipoPreparacion = dr["TipoPreparacion"].ToString()
+                            });
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    lista = new List<Tipo_Platillo>();
+                }
+            }
+
+            return lista;
+        }
+
+        public bool Registrar(Tipo_Platillo obj)
+        {
+            bool respuesta = false;
+
+            using (MySqlConnection conexion = new MySqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    string query = @"INSERT INTO tipo_platillo
+                        (Id_Platillo, Tamano, TipoPreparacion)
+                        VALUES
+                        (@Id_Platillo, @Tamano, @TipoPreparacion)";
+
+                    MySqlCommand cmd = new MySqlCommand(query, conexion);
+
+                    cmd.Parameters.AddWithValue("@Id_Platillo", obj.Id_Platillo);
+                    cmd.Parameters.AddWithValue("@Tamano", obj.Tamaño);
+                    cmd.Parameters.AddWithValue("@TipoPreparacion", obj.TipoPreparacion);
+
+                    conexion.Open();
+                    respuesta = cmd.ExecuteNonQuery() > 0;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    respuesta = false;
+                }
+            }
+
+            return respuesta;
+        }
+
+        public bool Editar(Tipo_Platillo obj)
+        {
+            bool respuesta = false;
+
+            using (MySqlConnection conexion = new MySqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    string query = @"UPDATE tipo_platillo
+                        SET Id_Platillo = @Id_Platillo,
+                            Tamano = @Tamano,
+                            TipoPreparacion = @TipoPreparacion
+                        WHERE Id_Tipo = @Id_Tipo";
+
+                    MySqlCommand cmd = new MySqlCommand(query, conexion);
+
+                    cmd.Parameters.AddWithValue("@Id_Tipo", obj.Id_Tipo);
+                    cmd.Parameters.AddWithValue("@Id_Platillo", obj.Id_Platillo);
+                    cmd.Parameters.AddWithValue("@Tamano", obj.Tamaño);
+                    cmd.Parameters.AddWithValue("@TipoPreparacion", obj.TipoPreparacion);
+
+                    conexion.Open();
+                    respuesta = cmd.ExecuteNonQuery() > 0;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    respuesta = false;
+                }
+            }
+
+            return respuesta;
+        }
+
+        public bool Eliminar(int idTipo)
+        {
+            bool respuesta = false;
+
+            using (MySqlConnection conexion = new MySqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    string query = "DELETE FROM tipo_platillo WHERE Id_Tipo = @Id_Tipo";
+
+                    MySqlCommand cmd = new MySqlCommand(query, conexion);
+                    cmd.Parameters.AddWithValue("@Id_Tipo", idTipo);
+
+                    conexion.Open();
+                    respuesta = cmd.ExecuteNonQuery() > 0;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    respuesta = false;
+                }
+            }
+
+            return respuesta;
+        }
+
+        public bool ExistePlatillo(int idPlatillo)
+        {
+            using (MySqlConnection conexion = new MySqlConnection(Conexion.cadena))
+            {
+                string query = "SELECT 1 FROM platillo WHERE Id_Platillo = @Id LIMIT 1";
+
+                MySqlCommand cmd = new MySqlCommand(query, conexion);
+                cmd.Parameters.AddWithValue("@Id", idPlatillo);
+
+                conexion.Open();
+
+                return cmd.ExecuteScalar() != null;
+            }
+        }
+
+        public bool ExisteTipo(int idTipo)
+        {
+            using (MySqlConnection conexion = new MySqlConnection(Conexion.cadena))
+            {
+                string query = "SELECT 1 FROM tipo_platillo WHERE Id_Tipo = @Id LIMIT 1";
+
+                MySqlCommand cmd = new MySqlCommand(query, conexion);
+                cmd.Parameters.AddWithValue("@Id", idTipo);
+
+                conexion.Open();
+
+                return cmd.ExecuteScalar() != null;
+            }
+        }
     }
 }
--- CapaValidar/CV_Tipo_Platillo.cs
+++ CapaValidar/CV_Tipo_Platillo.cs
@@ -3,10 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CapaLogica;
+using CapaMo;
 
 namespace CapaValidar
 {
     internal class CV_Tipo_Platillo
     {
+        private CL_Tipo_Platillo datos = new CL_Tipo_Platillo();
+
+        public List<Tipo_Platillo> Listar()
+        {
+            return datos.Listar();
+        }
+
+        public bool Registrar(Tipo_Platillo obj, out string mensaje)
+        {
+            mensaje = "";
+
+            if (obj.Id_Platillo <= 0)
+                mensaje += "El platillo es obligatorio\n";
+            else if (!datos.ExistePlatillo(obj.Id_Platillo))
+                mensaje += "El platillo no existe\n";
+
+            if (string.IsNullOrWhiteSpace(obj.Tamaño))
+                mensaje += "El tamaño es obligatorio\n";
+
+            if (string.IsNullOrWhiteSpace(obj.TipoPreparacion))
+                mensaje += "El tipo de preparación es obligatorio\n";
+
+            if (mensaje != "")
+                return false;
+
+            return datos.Registrar(obj);
+        }
+
+        public bool Editar(Tipo_Platillo obj, out string mensaje)
+        {
+            mensaje = "";
+
+            if (obj.Id_Tipo <= 0)
+                mensaje += "ID inválido\n";
+            else if (!datos.ExisteTipo(obj.Id_Tipo))
+                mensaje += "El tipo de platillo no existe\n";
+
+            if (obj.Id_Platillo <= 0)
+                mensaje += "El platillo es obligatorio\n";
+            else if (!datos.ExistePlatillo(obj.Id_Platillo))
+                mensaje += "El platillo no existe\n";
+
+            if (string.IsNullOrWhiteSpace(obj.Tamaño))
+                mensaje += "El tamaño es obligatorio\n";
+
+            if (string.IsNullOrWhiteSpace(obj.TipoPreparacion))
+                mensaje += "El tipo de preparación es obligatorio\n";
+
+            if (mensaje != "")
+                return false;
+
+            return datos.Editar(obj);
+        }
+
+        public bool Eliminar(int idTipo, out string mensaje)
+        {
+            mensaje = "";
+
+            if (idTipo <= 0)
+            {
+                mensaje = "ID inválido";
+                return false;
+            }
+
+            if (!datos.ExisteTipo(idTipo))
+            {
+                mensaje = "El tipo de platillo no existe";
+                return false;
+            }
+
+            return datos.Eliminar(idTipo);
+        }
     }
 }
