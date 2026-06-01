5a40d354b263772bdd9f60410deaea028a60e618
leonelmisael36-ux <leonemisael36@gmail.com>
2026-06-01 17:10:52 -0600

    si

--- Proyecto_Restaurante/Iniciar_Sesión.Designer.cs
+++ Proyecto_Restaurante/Iniciar_Sesión.Designer.cs
@@ -42,9 +42,10 @@
             this.Link.BackColor = System.Drawing.Color.Transparent;
             this.Link.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Link.LinkColor = System.Drawing.Color.Black;
-            this.Link.Location = new System.Drawing.Point(89, 384);
+            this.Link.Location = new System.Drawing.Point(67, 272);
+            this.Link.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Link.Name = "Link";
-            this.Link.Size = new System.Drawing.Size(299, 20);
+            this.Link.Size = new System.Drawing.Size(240, 17);
             this.Link.TabIndex = 0;
             this.Link.TabStop = true;
             this.Link.Text = "No tienes cuenta?, Dame click";
@@ -52,18 +53,20 @@
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.Color.Moccasin;
-            this.textBox1.Location = new System.Drawing.Point(122, 178);
+            this.textBox1.Location = new System.Drawing.Point(92, 105);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(230, 22);
+            this.textBox1.Size = new System.Drawing.Size(174, 20);
             this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(119, 159);
+            this.label1.Location = new System.Drawing.Point(89, 89);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.Size = new System.Drawing.Size(46, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Usuario ";
             // 
@@ -71,43 +74,47 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Location = new System.Drawing.Point(119, 216);
+            this.label2.Location = new System.Drawing.Point(89, 136);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.Size = new System.Drawing.Size(61, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Contraseña";
             // 
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.Color.Moccasin;
-            this.textBox2.Location = new System.Drawing.Point(122, 253);
+            this.textBox2.Location = new System.Drawing.Point(92, 166);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(230, 22);
+            this.textBox2.Size = new System.Drawing.Size(174, 20);
             this.textBox2.TabIndex = 4;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.PapayaWhip;
-            this.button1.Location = new System.Drawing.Point(182, 310);
+            this.button1.Location = new System.Drawing.Point(136, 212);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(113, 23);
+            this.button1.Size = new System.Drawing.Size(85, 19);
             this.button1.TabIndex = 5;
             this.button1.Text = "Iniciar sesión";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // log_in
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Proyecto_Restaurante.Properties.Resources.Iniciar_sesión;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(482, 630);
+            this.ClientSize = new System.Drawing.Size(362, 365);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.Link);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "log_in";
             this.Text = "log_in";
             this.ResumeLayout(false);
